f39a1b61f870502bcd04a1858251e0477b986cd5
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-09 10:50:46 -0300

    obtendo cursos em destaque

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,18 @@ import { Request, Response } from "express"
 import { courseService } from "../services/courseService"
 
 export const  coursesController = {
+  //GET / courses / featured
+  featured: async (req: Request, res: Response) =>{
+    try{
+      const featuredCourses = await courseService.getRandomFeaturedCourses()
+      return res.json(featuredCourses)
+    }catch(error){
+      if (error instanceof Error){
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  },
+
   // GET / courses / :id
   show: async (req: Request, res: Response) =>{
     const { id } = req.params
@@ -14,5 +26,6 @@ export const  coursesController = {
       }
     }
   }
+  
 
 }
