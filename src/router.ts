e709b9b6490b9bb2972fbf9d5d2809ca99f6565e
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-09 09:49:18 -0300

    adicionando requisições por id

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import { categoriesController } from './controllers/categoriesController'
 const router  = express.Router()
 
 router.get('/categories',categoriesController.index)
+router.get('/categories/:id',categoriesController.show)
 
 
 export {router}
