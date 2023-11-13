34d517ec53ccb140d74dd4b40e03f8cc8776c3d4
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-13 18:19:53 -0300

    adicionando favoritos

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesController'
 import { authController } from './controllers/authController'
 import { ensureAuth, ensureAuthVideo } from './middlewares/auth'
+import { favoritesController } from './controllers/favoritesController'
 
 
 const router  = express.Router()
@@ -20,5 +21,7 @@ router.get('/courses/search', ensureAuth, coursesController.search)
 router.get('/courses/:id', ensureAuth, coursesController.show)
 
 router.get('/episodes/stream', ensureAuthVideo, episodesController.stream)
+
+router.post('/favorites',ensureAuth, favoritesController.save)
 
 export {router}
