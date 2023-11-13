e56cc1649aa98026d820cb0b1392b59ce4dcae90
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-13 17:27:44 -0300

    protegende os videos como token

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -17,14 +17,35 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
 
   const token = authorizationHeader.replace(/Bearer /, '')
 
-  jwtService.verifyToken(token, (err, decoded)=>{
+  jwtService.verifyToken(token, async (err, decoded)=>{
     if(err || typeof decoded === 'undefined') return res.status(401).json({
       message: 'Não autorizado: token invalido'
     })
 
-    userService.findByEmail((decoded as  JwtPayload).email).then(user => {
-      req.user = user
-      next()
+   const user = await userService.findByEmail((decoded as  JwtPayload).email)
+    req.user = user
+    next()
+  })
+}
+
+export function ensureAuthVideo(req: AuthenticatedRequest, res: Response, next: NextFunction){
+  const { token } = req.query
+
+  if (!token) return res.status(401).json({
+    message: 'Não autorizado: nenhum token foi encontrado'
+  })
+
+  if (typeof token !== 'string') return res.status(400).json({
+    message: 'O parâmetro token deve ser do tipo string'
+  })
+
+  jwtService.verifyToken(token, async (err, decoded)=>{
+    if(err || typeof decoded === 'undefined') return res.status(401).json({
+      message: 'Não autorizado: token invalido'
     })
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
+    req.user = user
+    next()
   })
 }
